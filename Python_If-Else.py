f1f7c3e8bc70ea7c1ea7957d813458ad58db4063
Saatvik <saatvikms05@gmail.com>
2022-12-02 20:35:52 +0530

    initial commit

---
+++ Python_If-Else.py
@@ -0,0 +1,27 @@
+"""
+   Python If-Else
+   -Hacker-rank
+   -https://www.hackerrank.com/challenges/py-if-else/problem
+"""
+n = int(input())
+
+if type(n) != int:
+    print('Enter a number')
+
+else:
+    if(n%2 == 1):
+        print('Weird')
+    elif(n>=2 and n<=5):
+         print('Not Weird')
+    elif(n>=6 and n<=20):
+        print('Weird')
+    else:
+        print('Not Weird')
+
+    
+
+
+
+
+
+
